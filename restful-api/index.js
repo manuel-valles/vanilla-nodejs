d840cee3bd180d618d5767d8b37d6d1254a7cfbe
Manuel Valles <manuel.valles@beamery.com>
2022-08-21 18:42:38 +0100

    Parsing query strings & HTTP methods

--- restful-api/index.js
+++ restful-api/index.js
@@ -4,14 +4,23 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
   // Get the URL and parse it
-  const parsedUrl = url.parse(req.url);
+  const parsedUrl = url.parse(req.url, true);
 
   // Get the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
   res.end('Hello World!\n');
-  console.log(`Request received on path: ${trimmedPath}`);
+  console.log(
+    `Request received on path '${trimmedPath}' with method '${method}' and query string:`,
+    queryStringObject
+  );
 });
 
 const PORT = process.env.PORT || 3000;
