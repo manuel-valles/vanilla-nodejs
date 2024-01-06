88c2b46ce5ba91e3c15e093e38d882b8a926500c
Manuel Valles <manuel.valles@beamery.com>
2024-01-06 12:06:25 +0000

    Routing requests

--- restful-api/index.js
+++ restful-api/index.js
@@ -22,16 +22,37 @@ const server = http.createServer((req, res) => {
 
   // Get the payload
   const decoder = new stringDecoder('utf-8');
-  let buffer = '';
-  req.on('data', data => buffer += decoder.write(data));
+  let payload = '';
+  req.on('data', data => payload += decoder.write(data));
   req.on('end', () => {
-    buffer += decoder.end();
+    payload += decoder.end();
 
-    // Send the response
-    res.end('Hello World!\n');
+    // Choose the handler
+    const chosenHandler = router[trimmedPath] ?? handlers.notFound;
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode === 'number' ? statusCode : 200;
+      payload = typeof payload === 'object' ? payload : {};
+      const payloadString = JSON.stringify(payload);
+      // Send the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log('Returning this response:', statusCode, payloadString);
+    });
 
     console.log('Request received with the headers:', headers);
-    console.log('Request received with the payload:', buffer);
+    console.log('Request received with the payload:', payload);
     console.log(
       `Request received on path '${trimmedPath}' with method '${method}' and query string:`,
       queryStringObject
@@ -41,6 +62,22 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+// Start the server
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+// Define the handlers
+const handlers = {};
+handlers.sample = (data, callback) => {
+  // Callback a http status code, and a payload object
+  callback(406, { 'name': 'sample handler' })
+};
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// Define a request router (mapping between the path and the handler)
+const router = {
+  'sample': handlers.sample
+};
