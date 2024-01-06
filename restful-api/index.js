0ec1e80af95c8cb71cba85b07b2530e26cff300f
Manuel Valles <manuel.valles@beamery.com>
2024-01-06 16:20:36 +0000

    Storing data through file system

--- restful-api/index.js
+++ restful-api/index.js
@@ -5,6 +5,13 @@ const url = require('url');
 const stringDecoder = require('string_decoder').StringDecoder;
 const fs = require('fs');
 const { httpPort, httpsPort, envName } = require('./config');
+const { create, read, update, remove } = require('./lib/data');
+
+// Testing
+// create('test', 'newFile', { foo: 'bar' }, err => console.log({ err }));
+read('test', 'newFile', (err, data) => console.log({ err, data }));
+// update('test', 'newFile', { fizz: 'buzz' }, err => console.log({ err }));
+// remove('test', 'newFile', err => console.log({ err }));
 
 // Instantiate the HTTP server
 const httpServer = http.createServer((req, res) => unifiedServer(req, res));
@@ -32,13 +39,13 @@ handlers.sample = (data, callback) => {
   // Callback a http status code, and a payload object
   callback(406, { 'name': 'sample handler' })
 };
-handlers.notFound = (data, callback) => {
-  callback(404);
-};
+handlers.ping = (data, callback) => callback(200);
+handlers.notFound = (data, callback) => callback(404);
 
 // Define a request router (mapping between the path and the handler)
 const router = {
-  'sample': handlers.sample
+  sample: handlers.sample,
+  ping: handlers.ping,
 };
 
 // Handle http and https requests
---
+++ restful-api/lib/data.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+const create = (dir, file, data, callback) => {
+    // Open the file for writing
+    fs.open(path.join(__dirname, '../.data', dir, `${file}.json`), 'wx', (err, fileDescriptor) => {
+        if (err || !fileDescriptor) return callback(err);
+        // Convert data to string
+        const stringData = JSON.stringify(data);
+        // Write to file and close it
+        fs.writeFile(fileDescriptor, stringData, err => {
+            if (err) return callback(err);
+            fs.close(fileDescriptor, err => {
+                if (err) return callback(err);
+                callback(false);
+            });
+        });
+    });
+}
+
+const read = (dir, file, callback) => fs.readFile(path.join(__dirname, '../.data', dir, `${file}.json`), 'utf8', (err, data) => callback(err, data));
+
+const update = (dir, file, data, callback) => {
+    // Open the file for writing
+    fs.open(path.join(__dirname, '../.data', dir, `${file}.json`), 'r+', (err, fileDescriptor) => {
+        if (err || !fileDescriptor) return callback(err);
+        // Convert data to string
+        const stringData = JSON.stringify(data);
+        // Truncate the file
+        fs.ftruncate(fileDescriptor, err => {
+            if (err) return callback(err);
+            // Write to file and close it
+            fs.writeFile(fileDescriptor, stringData, err => {
+                if (err) return callback(err);
+                fs.close(fileDescriptor, err => {
+                    if (err) return callback(err);
+                    callback(false);
+                });
+            });
+        });
+    });
+}
+
+const remove = (dir, file, callback) => {
+    // Unlink the file
+    fs.unlink(path.join(__dirname, '../.data', dir, `${file}.json`), err => {
+        if (err) return callback(err);
+        callback(false);
+    });
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    remove,
+};;
