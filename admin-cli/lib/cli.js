7c07ea45a3fe2d7cfc0111d39455212a472c210a
Manuel Valles <manuel.valles@beamery.com>
2024-02-03 12:32:32 +0000

    Add command man/help

--- admin-cli/lib/cli.js
+++ admin-cli/lib/cli.js
@@ -5,6 +5,99 @@ const events = require('events');
 class CliEvents extends events {}
 const cliEvents = new CliEvents();
 
+// Create a vertical space
+const verticalSpace = (lines = 1) => console.log('\n'.repeat(lines));
+
+// Create a horizontal line across the screen
+const horizontalLine = () => {
+    // Get screen size
+    const width = process.stdout.columns || 80;
+    // Fill the line with dashes
+    const line = '-'.repeat(width);
+    console.log(line);
+};
+
+
+// Create centered text on the screen
+const centered = (str) => {
+    str = typeof (str) === 'string' && str.trim().length > 0 ? str.trim() : '';
+    // Get screen size
+    const width = process.stdout.columns || 80;
+    // Calculate the left padding
+    const leftPadding = Math.floor((width - str.length) / 2);
+    // Put in left padded spaces before the string itself
+    console.log(' '.repeat(leftPadding), str);
+};
+
+// Input handlers
+cliEvents.on('help', () => responsers.help());
+cliEvents.on('man', () => responsers.help());
+cliEvents.on('exit', () => responsers.exit());
+cliEvents.on('stats', () => responsers.stats());
+cliEvents.on('list users', () => responsers.listUsers());
+cliEvents.on('more user info', (str) => responsers.moreUserInfo(str));
+cliEvents.on('list checks', (str) => responsers.listChecks(str));
+cliEvents.on('more check info', (str) => responsers.moreCheckInfo(str));
+cliEvents.on('list logs', () => responsers.listLogs());
+cliEvents.on('more log info', (str) => responsers.moreLogInfo(str));
+
+// Responders
+const responsers = {
+    help: () => {
+        const commands = {
+            'help': 'Show this help page',
+            'man': 'Alias of the "help" command',
+            'exit': 'Kill the CLI and the app',
+            'stats': 'Get statistics on the underlying operating system and resource utilization',
+            'list users': 'Show a list of all the registered users in the system',
+            'more user info --{userId}': 'Show details of a specific user',
+            'list checks --up --down': 'Show a list of all the active checks in the system, including their state. The flag --up and --down are optional',
+            'more check info --{checkId}': 'Show details of a specified check',
+            'list logs': 'Show a list of all the log files available to be read',
+            'more log info --{filename}': 'Show details of a specified log file'
+        };
+
+        // Show a header for help page that is as wide as the screen
+        horizontalLine();
+        centered('CLI Manual');
+        horizontalLine();
+        verticalSpace(2);
+
+        // Show each command, followed by its explanation
+        Object.entries(commands).forEach(([key, value]) => {
+            const line = `\x1b[33m${key}\x1b[0m`;
+            const padding = 60 - line.length;
+            console.log(`${line}${' '.repeat(padding)}${value}`);
+            verticalSpace();
+        });
+
+        verticalSpace(1);
+        horizontalLine();
+    },
+    exit: () => process.exit(0),
+    stats: () => {
+        console.log('You asked for stats');
+    },
+    listUsers: () => {
+        console.log('You asked for list users');
+    },
+    moreUserInfo: (str) => {
+        console.log('You asked for more user info', str);
+    },
+    listChecks: (str) => {
+        console.log('You asked for list checks', str);
+    },
+    moreCheckInfo: (str) => {
+        console.log('You asked for more check info', str);
+    },
+    listLogs: () => {
+        console.log('You asked for list logs');
+    },
+    moreLogInfo: (str) => {
+        console.log('You asked for more log info', str);
+    }
+};
+
 const processInput = (str) => {
     str = typeof (str) === 'string' && str.trim().length > 0 && str.trim();
     // Only process the input if the user type something
@@ -35,47 +128,6 @@ const processInput = (str) => {
         if(!matchFound){
             console.log('Command not found, please try again');
         }
-    }
-};
-
-// Input handlers
-cliEvents.on('help', () => responsers.help());
-cliEvents.on('man', () => responsers.help());
-cliEvents.on('exit', () => responsers.exit());
-cliEvents.on('stats', () => responsers.stats());
-cliEvents.on('list users', () => responsers.listUsers());
-cliEvents.on('more user info', (str) => responsers.moreUserInfo(str));
-cliEvents.on('list checks', (str) => responsers.listChecks(str));
-cliEvents.on('more check info', (str) => responsers.moreCheckInfo(str));
-cliEvents.on('list logs', () => responsers.listLogs());
-cliEvents.on('more log info', (str) => responsers.moreLogInfo(str));
-
-// Responders
-const responsers = {
-    help: () => {
-        console.log('You asked for help');
-    },
-    exit: () => process.exit(0),
-    stats: () => {
-        console.log('You asked for stats');
-    },
-    listUsers: () => {
-        console.log('You asked for list users');
-    },
-    moreUserInfo: (str) => {
-        console.log('You asked for more user info', str);
-    },
-    listChecks: (str) => {
-        console.log('You asked for list checks', str);
-    },
-    moreCheckInfo: (str) => {
-        console.log('You asked for more check info', str);
-    },
-    listLogs: () => {
-        console.log('You asked for list logs');
-    },
-    moreLogInfo: (str) => {
-        console.log('You asked for more log info', str);
     }
 };
 
