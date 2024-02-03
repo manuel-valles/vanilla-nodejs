c8b991f85474f3b6c0907c49285d807b19a68411
Manuel Valles <manuel.valles@beamery.com>
2024-02-03 11:58:32 +0000

    Input handlers for CLI

--- admin-cli/lib/cli.js
+++ admin-cli/lib/cli.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 const util = require('util');
 const debug = util.debuglog('cli');
 const events = require('events');
-class CliEvents extends events { }
+class CliEvents extends events {}
 const cliEvents = new CliEvents();
 
 const processInput = (str) => {
@@ -33,8 +33,49 @@ const processInput = (str) => {
 
         // If no match is found, tell the user to try again
         if(!matchFound){
-            console.log('Sorry, try again');
+            console.log('Command not found, please try again');
         }
+    }
+};
+
+// Input handlers
+cliEvents.on('help', () => responsers.help());
+cliEvents.on('man', () => responsers.help());
+cliEvents.on('exit', () => responsers.exit());
+cliEvents.on('stats', () => responsers.stats());
+cliEvents.on('list users', () => responsers.listUsers());
+cliEvents.on('more user info', (str) => responsers.moreUserInfo(str));
+cliEvents.on('list checks', (str) => responsers.listChecks(str));
+cliEvents.on('more check info', (str) => responsers.moreCheckInfo(str));
+cliEvents.on('list logs', () => responsers.listLogs());
+cliEvents.on('more log info', (str) => responsers.moreLogInfo(str));
+
+// Responders
+const responsers = {
+    help: () => {
+        console.log('You asked for help');
+    },
+    exit: () => process.exit(0),
+    stats: () => {
+        console.log('You asked for stats');
+    },
+    listUsers: () => {
+        console.log('You asked for list users');
+    },
+    moreUserInfo: (str) => {
+        console.log('You asked for more user info', str);
+    },
+    listChecks: (str) => {
+        console.log('You asked for list checks', str);
+    },
+    moreCheckInfo: (str) => {
+        console.log('You asked for more check info', str);
+    },
+    listLogs: () => {
+        console.log('You asked for list logs');
+    },
+    moreLogInfo: (str) => {
+        console.log('You asked for more log info', str);
     }
 };
 
